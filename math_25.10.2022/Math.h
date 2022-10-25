40319014616afb45ae80efc8500339f264d4e3ea
dzemiachkovskii <fennyboy2012@gmail.com>
2022-10-25 10:52:39 +0300

    initial commit

---
+++ math_25.10.2022/Math.h
@@ -0,0 +1,17 @@
+#ifndef MATH_H
+#define MATH_H
+
+class Math {
+public:
+	int Sum(int, int);
+
+	int Sub(int, int);
+	
+	int Div(int, int);
+	
+	int Mul(int, int);
+	
+	int Mod(int, int);
+};
+
+#endif
