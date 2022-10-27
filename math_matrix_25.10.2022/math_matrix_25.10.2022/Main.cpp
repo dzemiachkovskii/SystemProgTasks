5d67d3544480a226cf8fbdd37a06d1c354507b52
dzemiachkovskii <fennyboy2012@gmail.com>
2022-10-27 10:28:41 +0300

    change raw files to VS project

---
+++ math_matrix_25.10.2022/math_matrix_25.10.2022/Main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include "MathMatrix.h"
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    MatrixMath m;
+    MatrixMisc t;
+
+    auto matrix = t.Generate(4, 5);
+    auto xirtam = t.Generate(5, 7);
+
+    std::cout << "Исходные массивы:" << std::endl;
+    t.Print(matrix);
+    t.Print(xirtam);
+
+    std::cout << "Сумма матриц:" << std::endl;
+    t.Print(m.Sum(matrix, xirtam));
+    std::cout << "Разность матриц:" << std::endl;
+    t.Print(m.Sub(matrix, xirtam));
+    std::cout << "Произведение матриц:" << std::endl;
+    t.Print(m.Mul(matrix, xirtam));
+    std::cout << "Частное матриц:" << std::endl;
+    t.Print(m.Div(matrix, xirtam));
+    std::cout << "Остаток от деления первой матрицы на вторую:" << std::endl;
+    t.Print(m.Mod(matrix, xirtam));
+}
